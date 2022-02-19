867c6399d81953c2c81d234d7ca2fbe1c22435a7
4523454554 <choikj33@gmail.com>
2022-02-19 19:32:08 +0900

    push

--- React Project Tutorial/management/src/App.js
+++ React Project Tutorial/management/src/App.js
@@ -1,6 +1,11 @@
 import Customer from './components/Customer';
 import './App.css';
 import { Component } from 'react';
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 
 const customers = [
 {
@@ -35,20 +40,22 @@ class App extends Component {
   render() {
     return (
       <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-                />
-            )
-          })
-        }
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+            <TableBody>
+            {customers.map(c => {return (<Customer key={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+            </TableBody>
+          
+        </Table>
       </div>
     );
   }
